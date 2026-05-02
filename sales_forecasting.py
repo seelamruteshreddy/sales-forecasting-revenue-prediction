c146a0e2d8c24ee4a65105a71093e1a685fb0006
Seelam Rutesh Reddy <ruteshreddyseelam@gmail.com>
2026-05-01 22:32:58 -0700

    Replace XGBoost with RandomForest for forecasting

--- sales_forecasting.py
+++ sales_forecasting.py
@@ -1,6 +1,26 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
-print("Sales Forecasting Project Started")
+data = {
+    "price": [10, 20, 30, 40, 50],
+    "sales": [100, 90, 80, 70, 60]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["price"]]
+y = df["sales"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("MAE:", mean_absolute_error(y_test, pred))
